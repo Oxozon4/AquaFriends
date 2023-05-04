039775c79918c2d669fa2ba52af6249632ea0a6d
Oxozon <oxozon.o4@gmail.com>
2023-05-04 23:30:18 +0200

    init React toastify

--- app-react/src/views/Root.tsx
+++ app-react/src/views/Root.tsx
@@ -4,6 +4,8 @@ import { theme } from '../assets/styles/theme';
 import { Wrapper } from './Root-styled';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '../components/templates/LandingPage/LandingPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Root = () => {
   return (
@@ -11,6 +13,18 @@ const Root = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Wrapper>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            limit={1}
+          />
           <Routes>
             <Route path="/" element={<LandingPage />} />
           </Routes>
