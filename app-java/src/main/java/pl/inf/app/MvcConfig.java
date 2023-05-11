07eb2f79159a2f824a2494e3210af58a233b30e7
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2023-05-11 16:24:28 +0200

    Move app-react

---
+++ app-java/src/main/java/pl/inf/app/MvcConfig.java
@@ -0,0 +1,10 @@
+package pl.inf.app;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+
+}
