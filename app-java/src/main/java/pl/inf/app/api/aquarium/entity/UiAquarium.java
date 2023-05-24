410ed3728c2a3a49f72247a68912f0989fd026c6
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2023-05-24 21:43:25 +0200

    Reformat code

--- app-java/src/main/java/pl/inf/app/api/aquarium/entity/UiAquarium.java
+++ app-java/src/main/java/pl/inf/app/api/aquarium/entity/UiAquarium.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 import pl.inf.app.api.accessorytype.entity.UiAccessoryType;
 import pl.inf.app.api.decoratortype.entity.UiDecoratorType;
 import pl.inf.app.api.fish.entity.UiFish;
-import pl.inf.app.bm.accessorytype.entity.AccessoryTypeBE;
-import pl.inf.app.bm.decoratortype.entity.DecoratorTypeBE;
 
 import java.util.HashSet;
 import java.util.Set;
--- app-java/src/main/java/pl/inf/app/api/aquariumtemplate/entity/UiAquariumTemplate.java
+++ app-java/src/main/java/pl/inf/app/api/aquariumtemplate/entity/UiAquariumTemplate.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.inf.app.api.accessorytype.entity.UiAccessoryType;
-import pl.inf.app.bm.accessorytype.entity.AccessoryTypeBE;
 
 import java.util.HashSet;
 import java.util.Set;
