88e80161087be4e86506a0121ded6ed9eb130408
Oxozon <oxozon.o4@gmail.com>
2023-05-27 01:45:40 +0200

    add Dashboard LinksCtx getUserLinks call

--- app-java/src/main/app-react/src/components/templates/Dashboard/Dashboard.tsx
+++ app-java/src/main/app-react/src/components/templates/Dashboard/Dashboard.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { LinksContext } from '../../../providers/LinksProvider';
+
 import Header from '../../molecules/Headers/Header/Header';
 import FormsListSection from '../../organisms/FormsListSection/FormsListSection';
 import FormCreationModal from '../../organisms/FormCreationModal/FormCreationModal';
@@ -7,6 +9,7 @@ import { DashboardWrapper } from './Dashboard-styled';
 import { createPortal } from 'react-dom';
 
 const Dashboard: React.FC = () => {
+  const LinksCtx = useContext(LinksContext);
   const [showModal, setShowModal] = useState(false);
 
   const onLogoutHandler = async () => {
@@ -20,6 +23,18 @@ const Dashboard: React.FC = () => {
   const onCreateNewHandler = () => {
     setShowModal(true);
   };
+
+  useEffect(() => {
+    if (
+      !LinksCtx ||
+      !LinksCtx.user ||
+      LinksCtx.user.isSet ||
+      LinksCtx.getUserLinks === null
+    ) {
+      return;
+    }
+    LinksCtx.getUserLinks();
+  }, [LinksCtx]);
 
   return (
     <>
