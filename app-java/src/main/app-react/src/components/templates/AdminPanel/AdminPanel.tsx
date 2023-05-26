94c52125067c47dededc8b1720fe8ea51eac88f5
Oxozon <oxozon.o4@gmail.com>
2023-05-27 01:45:23 +0200

    add AdminPage LinksCtx getAdminLinks call

--- app-java/src/main/app-react/src/components/templates/AdminPanel/AdminPanel.tsx
+++ app-java/src/main/app-react/src/components/templates/AdminPanel/AdminPanel.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useContext } from 'react';
+import { LinksContext } from '../../../providers/LinksProvider';
+
 import Header from '../../molecules/Headers/Header/Header';
 import Footer from '../../molecules/Footer/Footer';
 import { AdminPanelWrapper, AdminPanelContent } from './AdminPanel-styled';
 
 const AdminPanel = () => {
+  const LinksCtx = useContext(LinksContext);
+
   const onLogoutHandler = async () => {
     window.location.href = '/';
   };
+
+  useEffect(() => {
+    if (
+      !LinksCtx ||
+      !LinksCtx.admin ||
+      LinksCtx.admin.isSet ||
+      LinksCtx.getAdminLinks === null
+    ) {
+      return;
+    }
+    LinksCtx.getAdminLinks();
+  }, [LinksCtx]);
 
   return (
     <AdminPanelWrapper>
