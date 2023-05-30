bfd3da54af7b5aaeda281432259ab1b3ccc00487
Oxozon <oxozon.o4@gmail.com>
2023-05-30 14:08:18 +0200

    add data fetching on AdminPanel init

--- app-java/src/main/app-react/src/components/templates/AdminPanel/AdminPanel.tsx
+++ app-java/src/main/app-react/src/components/templates/AdminPanel/AdminPanel.tsx
@@ -1,27 +1,83 @@
-import { useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { LinksContext } from '../../../providers/LinksProvider';
 
 import Header from '../../molecules/Headers/Header/Header';
 import Footer from '../../molecules/Footer/Footer';
 import { AdminPanelWrapper, AdminPanelContent } from './AdminPanel-styled';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const AdminPanel = () => {
   const LinksCtx = useContext(LinksContext);
+
+  const [aquariumTemplates, setAquariumTemplates] = useState<any[]>([]);
+  const [knowledgeBase, setKnowledgeBase] = useState<any[]>([]);
+  const [fishTypes, setFishTypes] = useState<any[]>([]);
+  const [accessoryTypes, setAccessoryTypes] = useState<any[]>([]);
+  const [decoratorTypes, setDecoratorTypes] = useState<any[]>([]);
 
   const onLogoutHandler = async () => {
     window.location.href = '/';
   };
 
   useEffect(() => {
-    if (
-      !LinksCtx ||
-      !LinksCtx.admin ||
-      LinksCtx.admin.isSet ||
-      LinksCtx.getAdminLinks === null
-    ) {
+    const getAllAquariumTemplates = async () => {
+      if (!LinksCtx || !LinksCtx.admin) {
+        return;
+      }
+      const response = await axios.get(LinksCtx.admin.getAllAquariumTemplate);
+      setAquariumTemplates(response.data);
+    };
+
+    const getAllFishTypes = async () => {
+      if (!LinksCtx || !LinksCtx.admin) {
+        return;
+      }
+      const response = await axios.get(LinksCtx.admin.getAllFishType);
+      setFishTypes(response.data);
+    };
+
+    const getAllKnowledgeBase = async () => {
+      if (!LinksCtx || !LinksCtx.admin) {
+        return;
+      }
+      const response = await axios.get(LinksCtx.admin.getAllKnowledge);
+      setKnowledgeBase(response.data);
+    };
+
+    const getAllAccessoryTypes = async () => {
+      if (!LinksCtx || !LinksCtx.admin) {
+        return;
+      }
+      const response = await axios.get(LinksCtx.admin.getAllAccessoryType);
+      setAccessoryTypes(response.data);
+    };
+
+    const getAllDecoratorTypes = async () => {
+      if (!LinksCtx || !LinksCtx.admin) {
+        return;
+      }
+      const response = await axios.get(LinksCtx.admin.getAllDecoratorType);
+      setDecoratorTypes(response.data);
+    };
+
+    if (!LinksCtx || !LinksCtx.admin || LinksCtx.getAdminLinks === null) {
       return;
     }
-    LinksCtx.getAdminLinks();
+    if (LinksCtx.admin.isSet) {
+      try {
+        getAllAquariumTemplates();
+        getAllFishTypes();
+        getAllKnowledgeBase();
+        getAllAccessoryTypes();
+        getAllDecoratorTypes();
+      } catch (err) {
+        toast.error('Wystąpił błąd podczas pobierania danych');
+        console.warn(err);
+      }
+    } else {
+      LinksCtx.getAdminLinks();
+    }
   }, [LinksCtx]);
 
   return (
