38f7951dc13007c811941ee6bd0d61910a03b025
Oxozon <oxozon.o4@gmail.com>
2023-06-10 10:28:13 +0200

    add allAquariums data fetching

--- app-java/src/main/app-react/src/components/templates/Dashboard/Dashboard.tsx
+++ app-java/src/main/app-react/src/components/templates/Dashboard/Dashboard.tsx
@@ -1,13 +1,16 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useCallback } from 'react';
 import { LinksContext } from '../../../providers/LinksProvider';
 
 import Header from '../../molecules/Headers/Header/Header';
 import FormsListSection from '../../organisms/ItemListSection/ItemListSection';
 import Footer from '../../molecules/Footer/Footer';
 import { DashboardWrapper } from './Dashboard-styled';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Dashboard: React.FC = () => {
   const LinksCtx = useContext(LinksContext);
+  const [allAquariums, setAllAquariums] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
   const onLogoutHandler = async () => {
@@ -22,6 +25,24 @@ const Dashboard: React.FC = () => {
     setShowModal(true);
   };
 
+  const getAllAquariums = useCallback(async () => {
+    if (!LinksCtx || !LinksCtx.user || !LinksCtx.user.getAllAquarium) {
+      return;
+    }
+    const response = await axios.get(LinksCtx.user.getAllAquarium);
+    if (
+      !response.data ||
+      !response.data._embedded ||
+      !response.data._embedded.uiAquariumList
+    ) {
+      toast.error(
+        'Wystąpił problem z pobraniem listy akwariów. Spróbuj ponownie później!'
+      );
+      return;
+    }
+    setAllAquariums(response.data._embedded.uiAquariumList);
+  }, [LinksCtx]);
+
   useEffect(() => {
     if (
       !LinksCtx ||
@@ -34,6 +55,13 @@ const Dashboard: React.FC = () => {
     LinksCtx.getUserLinks();
   }, [LinksCtx]);
 
+  useEffect(() => {
+    if (!LinksCtx || !LinksCtx.user || !LinksCtx.user.isSet) {
+      return;
+    }
+    getAllAquariums();
+  }, [LinksCtx, getAllAquariums]);
+
   return (
     <>
       <DashboardWrapper>
@@ -45,8 +73,8 @@ const Dashboard: React.FC = () => {
         <FormsListSection
           onEditHandler={onEditHandler}
           onCreateNewHandler={onCreateNewHandler}
-          itemVariant={'aquariumTemplate'}
-          data={[]}
+          itemVariant={'aquarium'}
+          data={allAquariums}
         />
         <Footer />
       </DashboardWrapper>
